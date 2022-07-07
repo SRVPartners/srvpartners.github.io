afc5a12013c93a255649f882baee896b83c6383c
SRV <data@srvpartners.org>
2022-07-07 16:39:39 -0400

    Initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+	"use strict";
+    
+        /*==================================
+* Author        : "ThemeSine"
+* Template Name : Khanas HTML Template
+* Version       : 1.0
+==================================== */
+
+
+
+/*=========== TABLE OF CONTENTS ===========
+1. Scroll To Top 
+2. Smooth Scroll spy
+3. Progress-bar -- REMOVED
+4. owl carousel -- REMOVED
+5. welcome animation support -- REMOVED
+======================================*/
+
+    // 1. Scroll To Top 
+		$(window).on('scroll',function () {
+			if ($(this).scrollTop() > 600) {
+				$('.return-to-top').fadeIn();
+			} else {
+				$('.return-to-top').fadeOut();
+			}
+		});
+		$('.return-to-top').on('click',function(){
+				$('html, body').animate({
+				scrollTop: 0
+			}, 1500);
+			return false;
+		});
+	
+	
+	
+	// 2. Smooth Scroll spy
+		
+		$('.header-area').sticky({
+           topSpacing:0
+        });
+		
+		//=============
+
+		$('li.smooth-menu a').bind("click", function(event) {
+			event.preventDefault();
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top - 0
+			}, 1200,'easeInOutExpo');
+		});
+		
+		$('body').scrollspy({
+			target:'.navbar-collapse',
+			offset:0
+		});
+
+});	
+	
